a83e1d6d5ddb798c16de2e442e73aa53c9ea32ed
Eleven-k <Twinwik@qq.com>
2020-08-11 23:22:41 +0800

    翻译 URL 标示

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,9 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 // 分类列表话题
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
