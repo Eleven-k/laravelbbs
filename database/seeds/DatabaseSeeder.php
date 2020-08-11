8edae799d709ccf96df5889388f4fd22521dbb55
Eleven-k <Twinwik@qq.com>
2020-08-11 17:49:25 +0800

    填充用户和话题数据

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(TopicsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
