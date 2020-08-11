50409953786e2114986c3bc04ebb68903245b766
Eleven-k <Twinwik@qq.com>
2020-08-11 23:40:05 +0800

    回复数据

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,17 +1,11 @@
 <?php
-
 use Illuminate\Database\Seeder;
-
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        $this->call(TopicsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+        $this->call(TopicsTableSeeder::class);
+        $this->call(RepliesTableSeeder::class);
     }
 }
