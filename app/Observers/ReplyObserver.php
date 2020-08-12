bbab939f3eae6873c33e816af35e1682891563fb
Eleven-k <Twinwik@qq.com>
2020-08-12 11:29:49 +0800

    消息通知有新回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -1,14 +1,21 @@
 <?php
+
 namespace App\Observers;
+
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
+
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
+
 class ReplyObserver
 {
     public function created(Reply $reply)
     {
         $reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
+
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
 
     public function creating(Reply $reply)
